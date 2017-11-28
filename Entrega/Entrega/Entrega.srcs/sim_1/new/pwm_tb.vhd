66b8b9d90bc1b4d5c61b4f07fdb4b32e194d1389
eskri969 <krick96@gmail.com>
2017-11-28 13:21:26 +0100

    5.3v2

--- Entrega/Entrega/Entrega.srcs/sim_1/new/pwm_tb.vhd
+++ Entrega/Entrega/Entrega.srcs/sim_1/new/pwm_tb.vhd
@@ -18,6 +18,26 @@
 -- 
 ----------------------------------------------------------------------------------
 
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 23.11.2017 13:59:57
+-- Design Name: 
+-- Module Name: en_4_cycles_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -32,12 +52,36 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity pwm_tb is
---  Port ( );
 end pwm_tb;
 
 architecture Behavioral of pwm_tb is
+    Component pwm
+        Port ( clk_12megas : in STD_LOGIC;
+               reset : in STD_LOGIC;
+               en_2_cycles : in STD_LOGIC;
+               sample_in : in STD_LOGIC_VECTOR (7 downto 0);
+               sample_request : out STD_LOGIC;
+               pwm_pulse : out STD_LOGIC);
+    End component;
 
-begin
-
-
+    signal clk12 : std_logic := '0';
+    signal rst : std_logic := '1';
+    signal clk6: std_logic := '0';
+    signal ssample_in :  STD_LOGIC_VECTOR (7 downto 0):="00000000";
+    signal ssample_request : STD_LOGIC;
+    signal spwm_pulse :  STD_LOGIC;
+    
+    constant half_period1 : time := 8.33333333333333 ns;
+    constant half_period2 : time := 16.66666666666666 ns;
+    
+    begin
+        prueba: pwm port map(clk12,rst,clk6,ssample_in,ssample_request,spwm_pulse);--ERROR AQUI
+        clk12 <= not clk12 after half_period1;
+        clk6 <= not clk6 after half_period2;
+    process
+    begin
+        wait for 0.05 us; rst <= '0';
+        wait;
+    end process;
+        
 end Behavioral;
